247d2555947ccd93a6e37cb6510ceb68401312f8
Jesús Antonio Ugaz Vegas <jesusugazv@gmail.com>
2022-04-26 17:49:19 -0500

    Pruebas Unitarias

---
+++ tests/unit/pages/BrawlStarsPage.spec.js
@@ -0,0 +1,78 @@
+import { shallowMount, mount } from "@vue/test-utils"
+import BrawlStarsPage from "@/pages/BrawlStarsPage"
+
+import { mockBrawlstars } from "../mocks/brawlstarts.mock";
+
+
+describe('BrawlStarsPage component', () => {
+
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallowMount( BrawlStarsPage )
+        
+    })
+
+    test('debe de hacer match con el snapshot', () => {
+        expect( wrapper.html() ).toMatchSnapshot()    
+    
+    })
+
+    test('debe de llamar mixBrawlsArray al montar', () => {
+
+        const mixBrawlsArraySpy = jest.spyOn( BrawlStarsPage.methods, 'mixBrawlsArray' )
+        const wrapper = shallowMount( BrawlStarsPage )
+
+        expect( mixBrawlsArraySpy ).toHaveBeenCalled() 
+    })
+
+    test('debe de hacer match con el snapshot cuando cargan los brawls',() => {
+    
+        const wrapper = shallowMount( BrawlStarsPage, {
+
+            data() {
+                return {
+                    brawlStartArr: mockBrawlstars,
+                    brawl: mockBrawlstars[0],
+                    showBrawlStart: false,
+                    showAnswer:false,
+                    message:'',
+                }
+            },
+        
+        })
+
+        expect( wrapper.html() ).toMatchSnapshot()  
+    
+    })
+    
+    test('debe de mostrar los componentes de BrawlStarsPicutre y BrawlStarsOptions',() => {
+    
+
+
+          const wrapper = shallowMount( BrawlStarsPage, {
+
+            data() {
+                return {
+                    brawlStartArr: mockBrawlstars,
+                    brawl: mockBrawlstars[0],
+                    showBrawlStart: false,
+                    showAnswer:false,
+                    message:'',
+                }
+            },
+        })
+
+        const picture = wrapper.find('brawl-stars-picutre-stub')
+        const options = wrapper.find('brawl-stars-options-stub')
+
+        expect(picture.exists()).toBeTruthy()
+        expect(options.exists()).toBeTruthy()
+
+        expect(picture.attributes('brawlstartid')).toBe("1")
+        expect(options.attributes('brawls')).toBeTruthy()
+    })
+    
+    
+
+})
